7d06e5e6e44c3ac0a9e475a8a057da05c444badd
icedvariables <macodeblacko@gmail.com>
2015-06-27 10:57:29 +0100

    Verify blocks work

--- lyricfinder.py
+++ lyricfinder.py
@@ -2,6 +2,6 @@ import urllib2
 from lyricpage import LyricPage
 import pprint
 
-l = LyricPage(urllib2.urlopen("http://www.azlyrics.com/lyrics/slipknot/duality.html").read())
+l = LyricPage(urllib2.urlopen("http://www.azlyrics.com/lyrics/eminem/rapgod.html").read())
 print l.song, "by", l.author, "\n"
 pprint.pprint(l.lyrics.blocks)
